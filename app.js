d76e7524539d82920cae83e6b75039e8e3f3325f
TaylorMade <warriorotk18@gmail.com>
2022-08-05 23:29:17 -0600

    applied shadowblade page link to second button

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 function darkSwordPage() {
-	window.location='https://finalfantasy.fandom.com/wiki/Final_Fantasy_IV_weapons#Dark_swords', '_blank';
+	window.location='https://finalfantasy.fandom.com/wiki/Final_Fantasy_IV_weapons#Dark_swords';
+}
+function shadowBlade() {
+	window.location="https://finalfantasy.fandom.com/wiki/Shadowblade_(weapon)";
 }
