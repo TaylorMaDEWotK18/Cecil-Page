a718b2b758efb3dd3101e304368fc6504499e8e6
TaylorMade <warriorotk18@gmail.com>
2022-08-05 23:33:33 -0600

    made final weapon link to third button and set better page for darksword link

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 function darkSwordPage() {
-	window.location='https://finalfantasy.fandom.com/wiki/Final_Fantasy_IV_weapons#Dark_swords';
+	window.location='https://finalfantasy.fandom.com/wiki/Dark_Sword_(weapon)';
 }
 function shadowBlade() {
 	window.location="https://finalfantasy.fandom.com/wiki/Shadowblade_(weapon)";
+}
+function deathBringer() {
+	window.location="https://finalfantasy.fandom.com/wiki/Deathbringer";
 }
