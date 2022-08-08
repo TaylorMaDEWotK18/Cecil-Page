7991ea0a8cf24a429ca8019187c1bbad5caf0ef9
TaylorMade <warriorotk18@gmail.com>
2022-08-07 23:43:03 -0600

    created event listener function for form visibility

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ function deathBringer() {
 }
 
 const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+	const form = document.getElementById('form');
+
+	if (form.style.display === 'none') {
+		form.style.display = 'block';
+	} else {
+		form.style.display = 'none';
+	}
+});
